cc7fdd45e5f20e493ebfc5931c5cfa47ca4deb42
Harshsingh656 <hardbh56@gmail.com>
2024-12-15 14:30:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPoem() {
+    document.getElementById('poemPopup').style.display = 'block';
+}
+
+function hidePoem() {
+    document.getElementById('poemPopup').style.display = 'none';
+}
